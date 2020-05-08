88057468736e9622a36d5d303de4b4edf05b63fa
Rajanish <rajanishatwork@gmail.com>
2020-05-09 04:16:18 +0530

    game board layout defined
    Signed-off-by: Rajanish <rajanishatwork@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatGridListModule} from '@angular/material/grid-list';
+
 import { CosmosComponent } from './cosmos/cosmos.component';
 import { GameModule } from './game/game.module';
 
@@ -16,7 +19,8 @@ import { GameModule } from './game/game.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    GameModule
+    GameModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GameControllerPanelComponent } from './game-controller-panel/game-controller-panel.component';
-import { GameContainerComponent } from './game-container/game-container.component';
 
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider'; 
+import {MatDividerModule} from '@angular/material/divider';
+
+import { GameDetailsComponent } from './game-details/game-details.component';
+import { GameControllerPanelComponent } from './game-controller-panel/game-controller-panel.component';
+import { GameContainerComponent } from './game-container/game-container.component';
 
 
 @NgModule({
   declarations: [
     GameControllerPanelComponent,
-    GameContainerComponent
+    GameContainerComponent,
+    GameDetailsComponent
   ],
   imports: [
     CommonModule,
